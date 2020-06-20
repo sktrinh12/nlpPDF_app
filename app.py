af62b43c1fbbd2f088a0845f6878b7fe60138fb5
Spencer Trinh <trinh.kcs85@gmail.com>
2020-06-20 13:15:31 -0400

    type: feat-added flask flash msg
    Added flask flash messagining when file type is not correct, file size is too large, no file selected scenarios. Will redirect to home page and provide code=302.

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from werkzeug.utils import secure_filename
 from functions import *
 
+title = 'NLP PDF journal article keyword extraction'
+
 @app.route('/')
 @app.route('/index.html')
 def index():
-    return render_template('index.html', the_title='NLP PDF journal article keyword extraction')
+    return render_template('index.html', the_title=title)
 
 @app.route("/upload-pdf", methods=["GET", "POST"])
 def upload_pdf():
@@ -13,37 +15,37 @@ def upload_pdf():
             if not allowed_filesize(request.cookies.get("filesize")): #based on filesize
                 msg = 'File exceeded maximum size'
                 print(msg)
-                return redirect(request.url)
+                flash(msg, 'warning')
+                return redirect('/', code=302)
 
             pdf_file = request.files["pdf_up"]
 
             if pdf_file.filename == "": #no file name
-                msg = "File must have a filename"
+                msg = "Must select a file"
                 print(msg)
-                return redirect(request.url)
+                flash(msg, 'warning')
+                return redirect('/', code=302)
 
-            if not allowed_file(pdf_file.filename): #file type
-                msg = 'That file is not acceptable'
-                print(msg)
-                return redirect(request.url)
-            else:
+            if allowed_file(pdf_file.filename): #file type
                 filename = secure_filename(pdf_file.filename)
                 filepath = os.path.join(app.config['PDF_UPLOADS'], filename)
                 pdf_file.save(filepath)
-
-            msg = f"pdf file saved ... {pdf_file}"
-            print(msg)
-
-            sci_output, en_output = tokenize_render(filepath, \
-                                                    'Long-version keyword extraction', \
-                                                    'Short-version keyword extraction', \
-                                                    'ENTITY')
-
-    return render_template("upload_pdf.html", \
-            sci_output=sci_output, \
-            en_output=en_output, \
-            the_title='NLP PDF journal article keyword extraction')
-
+                msg = f"pdf file saved ... {pdf_file}"
+                print(msg)
+                sci_output, en_output = tokenize_render(filepath, \
+                                'Long-version keyword extraction', \
+                                'Short-version keyword extraction', \
+                                'ENTITY')
+                return render_template("upload_pdf.html", \
+                        sci_output=sci_output, \
+                        en_output=en_output, \
+                        the_title='NLP PDF journal article keyword extraction')
+            else:
+                msg = 'That file is not acceptable, should be .txt or .pdf'
+                flash(msg, 'warning')
+                print(msg)
+                return redirect('/', code=302)
+        return render_template('index.html',the_title=title)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
