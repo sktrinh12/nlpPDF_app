a21cce56fa0a0a7e13e67373d6fb08945de68cda
Spencer Trinh <trinh.kcs85@gmail.com>
2020-06-20 13:18:04 -0400

    type: feat-added if-else statement for env var
    If the ENV variable is production will inherit production class from
    config.py, otherwise will run development parameters; also print to
    console the current env being used

--- init.py
+++ init.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, request, redirect, jsonify, make_response
+from flask import Flask, render_template, request, redirect, flash#, jsonify, make_response
 from flaskext.markdown import Markdown
 
 app = Flask(__name__)
-Markdown(app)
+Markdown(app) #in order to render the displacy render
 cwd = app.root_path
+
+if app.config["ENV"] == "production":
+    app.config.from_object("config.ProductionConfig")
+else:
+    app.config.from_object("config.DevelopmentConfig")
+
+print(f'\n**ENV is set to: {app.config["ENV"]}**\n')
 
 app.config["PDF_UPLOADS"] = f"{cwd}/static/uploads"
 app.config["ALLOWED_FILE_EXT"] = ["PDF", "TXT"]
