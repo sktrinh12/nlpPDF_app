9d18e98ce195311441f4df3b517a0ea9573a9e73
Spencer Trinh <trinh.kcs85@gmail.com>
2020-07-22 01:20:06 -0400

    refactor: removed redirect
    removed redirect statements since flash messages were not working;
    removed the statement and added additional conditions in the if-else
    statements in order to prevent double flash messages

--- app.py
+++ app.py
@@ -8,51 +8,6 @@ title = 'NLP PDF journal article keyword extraction'
 def index():
     return render_template('index.html', the_title=title)
 
-#@app.route("/upload-pdf2", methods=["GET", "POST"])
-#def upload_pdf():
-#    if request.method == "POST":
-#        if request.files and "filesize" in request.cookies:
-#            if not allowed_filesize(request.cookies.get("filesize")):
-#                #based on filesize
-#                msg = f'File exceeded maximum size {request.cookies.get("filesize")}'
-#                print(msg)
-#                flash(msg, 'warning')
-#                return redirect('/', code=302)
-
-#            pdf_file = request.files["pdf_up"]
-
-#            if pdf_file.filename == "": #no file name
-#                msg = "Must select a file"
-#                print(msg)
-#                flash(msg, 'warning')
-#                return redirect('/', code=302)
-
-#            if allowed_file(pdf_file.filename): #file type
-#                filename = secure_filename(pdf_file.filename)
-#                filepath = os.path.join(app.config['PDF_UPLOADS'], filename)
-#                pdf_file.save(filepath)
-#                msg = f"pdf file saved ... {pdf_file}"
-#                print(msg)
-
-#                dict_output = tokenize_render(filepath, \
-#                                'long-version keyword extraction', \
-#                                'short-version keyword extraction'
-#                                )
-
-#                return render_template("upload_pdf.html", \
-#                        sci_output=dict_output['sci_output'], \
-#                        en_output=dict_output['en_output'], \
-#                        filename=filename, \
-#                        nscs=dict_output['nscs'], \
-#                        pg_no=dict_output['pg_no'], \
-#                        the_title=title)
-#            else:
-#                msg = 'That file is not acceptable, should be .txt or .pdf'
-#                flash(msg, 'warning')
-#                print(msg)
-#                return redirect('/', code=302)
-#        return render_template('index.html',the_title=title)
-
 @app.route("/test", methods=["GET", "POST"])
 def test():
     poss_wp, poss_nscs = [
@@ -73,9 +28,16 @@ def test():
                         "broad 23iu4hjljas",
                         "cars jaklsaf;",
                         "box alksjfa;ks",
-                        ], "N1238980"
+                        ], ["N1238980", "C891324", "J9834508", "M812703489", "N7104382"]
 
-    return render_template("upload_pdf2.html", \
+    pdf_file = ''
+    if pdf_file == "": #no file name
+        msg = "Must select a file"
+        print(msg)
+        flash(msg, 'warning')
+        # return redirect('/', code=302)
+
+    return render_template("upload_pdf.html", \
             filename='test_file.file', \
             poss_wp=poss_wp, \
             nscs=poss_nscs, \
@@ -85,12 +47,14 @@ def test():
 def upload_pdf():
     if request.method == "POST":
         if request.files and "filesize" in request.cookies:
-            if not allowed_filesize(request.cookies.get("filesize")):
+            print(f'filesize={request.cookies.get("filesize")}')
+            check_file_size = allowed_filesize(request.cookies.get("filesize"))
+            if not check_file_size:
                 #based on filesize
-                msg = f'File exceeded maximum size {request.cookies.get("filesize")}'
+                msg = f'File exceeded maximum size, ({int(request.cookies.get("filesize"))/1e6:0.2f}MB > 5MB)'
                 print(msg)
                 flash(msg, 'warning')
-                return redirect('/', code=302)
+                # return redirect('/', code=302)
 
             pdf_file = request.files["pdf_up"]
 
@@ -98,9 +62,9 @@ def upload_pdf():
                 msg = "Must select a file"
                 print(msg)
                 flash(msg, 'warning')
-                return redirect('/', code=302)
+                # return redirect('/', code=302)
 
-            if allowed_file(pdf_file.filename): #file type
+            if allowed_file(pdf_file.filename) : #file type
                 filename = secure_filename(pdf_file.filename)
                 filepath = os.path.join(app.config['PDF_UPLOADS'], filename)
                 pdf_file.save(filepath)
@@ -109,17 +73,18 @@ def upload_pdf():
 
                 poss_wp, poss_nscs = tokenise_render_v2(filepath)
 
-                return render_template("upload_pdf2.html", \
+                return render_template("upload_pdf.html", \
                         filename=filename, \
                         poss_wp=poss_wp, \
                         nscs=poss_nscs, \
                         the_title=title)
             else:
                 msg = 'That file is not acceptable, should be .txt or .pdf'
-                flash(msg, 'warning')
-                print(msg)
-                return redirect('/', code=302)
+                if pdf_file.filename != "" and check_file_size: # hackish way to prevent double flash messasge
+                    flash(msg, 'warning')
+                    print(msg)
+                # return redirect('/', code=302)
         return render_template('index.html',the_title=title)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=8050)
