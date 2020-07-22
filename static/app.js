809dcef85c0a4c6f3bc0b44fa435764e3d3ef6c8
Spencer Trinh <trinh.kcs85@gmail.com>
2020-07-22 01:27:32 -0400

    added timeout and refresh detection

--- static/app.js
+++ static/app.js
@@ -11,6 +11,7 @@ function clear_fz(){ //set the filesize back to 0 again
 
 function close_btn(){ //close the flash message
   $('#flash-msg').html("");
+  clear_fz();
 };
 
 document.querySelector('.custom-file-input').addEventListener('change',function(e){ //show filename in the input
@@ -28,5 +29,28 @@ window.addEventListener("load", function () {
 // loader appears after click button and waiting for flask to finish
 function loading() {
     document.querySelector(".loader-wrapper").className = "loader-wrapper";
-    clear_fz();
+    setTimeout(clear_fz, 12000);
+};
+
+// loader appears if refresh button pressed (ctrl or cmd R)
+// document.addEventListener("keydown", function onEvent(event) {
+//     var f5key = 116;
+//     // if (event.key === "r" && event.ctrlKey) {
+//     //   console.log('ctrl R pressed');
+//     // }
+//     // event.metaKey read-only property returning a Boolean that indicates if the 
+//     // Meta key was pressed (true) or not (false) when the event occurred
+//     if (event.key === "r" && event.keyCode <= 90 || 
+//         event.key === "r" && event.ctrlKey ||
+//         event.which == f5key) {
+//     var char = (event.metaKey ? '⌘-' : '') + String.fromCharCode(event.keyCode)
+//     console.log('<kbd>' + char + '</kbd>' + ' refreshing ...');
+//     loading();
+//   }
+// });
+
+// use of onbeforeunload to detect refresh 
+function loader_refresh() {
+  loading();
+  console.log('refresh button clicked');
 };
