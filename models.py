875acefeee72d236bb259376d126ae56e2f89681
Spencer Trinh <trinh.kcs85@gmail.com>
2020-07-22 14:07:52 -0400

    removed comments

--- models.py
+++ models.py
@@ -1,18 +1,5 @@
-# from sklearn.preprocessing import LabelEncoder #dependency for other libraries below
-# from nltk.classify.scikitlearn import SklearnClassifier
-# from sklearn.neighbors import KNeighborsClassifier
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn.ensemble import RandomForestClassifier
-# from sklearn.linear_model import LogisticRegression, SGDClassifier
-# from sklearn.naive_bayes import MultinomialNB, BernoulliNB, ComplementNB
-# from sklearn.naive_bayes import BernoulliNB
-# from sklearn.svm import SVC, LinearSVC, NuSVC
-# from sklearn.svm import LinearSVC
-#from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
-# from nltk.classify import ClassifierI
 from statistics import mode, StatisticsError
 from init import *
-# import pickle
 import joblib
 import numpy as np
 import pandas as pd
