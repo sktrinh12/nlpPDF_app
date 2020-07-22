d39b269dafa88f486fadc2b9c4132f48e2c5fd37
Spencer Trinh <trinh.kcs85@gmail.com>
2020-07-22 01:25:30 -0400

    refactor: changed max file size
    changed from 8MB to 5MB

--- init.py
+++ init.py
@@ -1,17 +1,10 @@
 from flask import Flask, render_template, request, redirect, flash#, jsonify, make_response
-from flaskext.markdown import Markdown
 
 app = Flask(__name__)
-Markdown(app) #in order to render the displacy render
 cwd = app.root_path
 
-if app.config["ENV"] == "production":
-    app.config.from_object("config.ProductionConfig")
-else:
-    app.config.from_object("config.DevelopmentConfig")
-
-print(f'\n**ENV is set to: {app.config["ENV"]}**\n')
+app.config.from_object("config.ProductionConfig")
 
 app.config["PDF_UPLOADS"] = f"{cwd}/static/uploads"
 app.config["ALLOWED_FILE_EXT"] = ["PDF", "TXT"]
-app.config["MAX_FILESIZE"] = 8 * 1024 * 1024#bytes 
+app.config["MAX_FILESIZE"] = 5 * 1024 * 1024#bytes (~5MB)
